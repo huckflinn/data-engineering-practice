bf2a5cc5e38ac2786460317d4bda2f910d81e657
Nicholas Flinn <huckflinn@users.noreply.github.com>
2025-04-26 15:10:47 -0400

    Add initial functionality.

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,6 +1,8 @@
 import requests
 import logging
+import zipfile
 from pathlib import Path
+from io import BytesIO
 
 logging.basicConfig(
     format="{asctime} | {levelname} | {message}",
@@ -19,36 +21,26 @@ download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2220_Q1.zip",
 ]
 
-
-# Once you have finished the project or want to test run your code,
-# run the following command docker-compose up run from inside the Exercises/Exercise-1 directory
-
-# Problems Statement
-# You need to download 10 files that are sitting at the following specified HTTP urls.
-# You will use the Python package requests to do this work.
-# You will need to pull the filename from the download uri.
-# The files are zip files that will also need to be unzipped into their csv format.
-# They should be downloaded into a folder called downloads which does not exist currently inside the Exercise-1 folder.
-# You should use Python to create the directory, do not do it manually.
 # Generally, your script should do the following ...
 # create the directory downloads if it doesn't exist
 # download the files one by one.
 # split out the filename from the uri, so the file keeps its original filename.
 # Each file is a zip, extract the csv from the zip and delete the zip file.
 # For extra credit, download the files in an async manner using the Python package aiohttp.
-# Also try using ThreadPoolExecutor in Python to download the files. Also write unit tests to improve your skills.
+# Also try using ThreadPoolExecutor in Python to download the files.
+# Also write unit tests to improve your skills.
 # Download URIs are listed in the main.py file.
 # Hints
 # Don't assume all the uri's are valid.
-# One approach would be the Python method split() to retrieve filename for uri, or maybe find the last occurrence of / and take the rest of the string.
+# One approach would be the Python method split() to retrieve filename for uri,
+# or maybe find the last occurrence of / and take the rest of the string.
 
 
-def downloads_dir_exists():
+def downloads_dir_exists(path):
     logging.info("Checking for downloads subdirectory...")
 
     # Check current directory for downloads folder.
-    p = Path.cwd()
-    for item in p.iterdir():
+    for item in path.iterdir():
 
         # If downloads folder is found, break the loop.
         if item.is_dir() and item.name == "downloads":
@@ -60,19 +52,86 @@ def downloads_dir_exists():
         logging.info("No downloads folder not found. Creating downloads folder...")
         Path("downloads").mkdir()
         logging.info("Created downloads folder.")
+
     except FileExistsError:
         logging.critical("Downloads folder already exists.")
+        raise
+
+    except Exception as e:
+        logging.critical(f"An unexpected error occurred: {e}")
+        raise
+
+
+def get_data(uri, path):
+    try:
+        logging.info(f"Retrieving data from {uri}...")
+        data = requests.get(uri)
+
+        if data.status_code == 200:
+            try:
+                with zipfile.ZipFile(BytesIO(data.content)) as z:
+                    logging.info("Attempting to unpack zip archive...")
+                    csv_name = get_csv_name(uri)
+                    
+                    csv_path = path / "downloads" / csv_name
+
+                    exists = csv_path.exists()
+
+                    if not exists:
+                        if csv_in_zip_archive(z, csv_name):
+                            z.extract(csv_name, path = path / "downloads")
+                            logging.info(f"{csv_name} successfully extracted.")
+                    else:
+                        logging.info(f"{csv_name} already exists.")
+
+            except zipfile.BadZipFile as bzf:
+                logging.error(f"Error encountered: {z} is a bad zip file.")
+                logging.error(f"{bzf}")
+
+    # consider adding logs for non-2xx HTTP status codes, maybe else: log status code + message
+    # confirm what HTTP/ConnectionError do, if the above
+    except requests.HTTPError:
+        # Try to print the specific error message as opposed to system-level stacktrace with raise
+        logging.critical("An HTTP error occurred.")
+        raise
+    except requests.ConnectionError:
+        logging.critical("A connection error occurred.")
         raise
     except Exception as e:
         logging.critical(f"An unexpected error occurred: {e}")
         raise
 
 
+def get_csv_name(uri):
+    z = uri.split("/")[-1]
+    return z.replace(".zip", ".csv")
+
+
+def csv_in_zip_archive(zip_archive, csv_name):
+    target = zipfile.Path(zip_archive) / csv_name
+    if target.exists():
+        logging.info(f"Found {csv_name} in zip archive.")
+        return True
+    else:
+        logging.debug(f"{csv_name} not in zip archive.")
+        return False
+
+
 def main():
     logging.info("Beginning execution.")
-    downloads_dir_exists()
+    path = Path()
+    try:
+        downloads_dir_exists(path)
+    except Exception as e:
+        logging.critical(f"Pipeline failed with an unexpected error: {e}")
+        raise
 
-    pass
+    for uri in download_uris:
+        try:
+            get_data(uri, path)
+        except Exception as e:
+            logging.critical(f"Pipeline failed with an unexpected error: {e}")
+            raise
 
 
 if __name__ == "__main__":
