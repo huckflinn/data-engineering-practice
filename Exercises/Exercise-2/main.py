e4ba4145d2540d44329d3da7415c41532a228e28
Nicholas Flinn <huckflinn@users.noreply.github.com>
2025-05-01 21:41:56 -0400

    Begin exercise 2

--- Exercises/Exercise-2/main.py
+++ Exercises/Exercise-2/main.py
@@ -1,9 +1,17 @@
 import requests
 import pandas
+from bs4 import BeautifulSoup
 
+def find_csv_filename(url, date):
+    html = requests.get(url)
+    soup = BeautifulSoup(html.content, "html.parser")
+    target = soup.find("td", string = date)
+    return target.find_previous_sibling().a.text
 
 def main():
-    # your code here
+    url = "https://www.ncei.noaa.gov/data/local-climatological-data/access/2021/"
+    date = "2024-01-19 10:27  "
+    print(find_csv_filename(url, date))
     pass
 
 
