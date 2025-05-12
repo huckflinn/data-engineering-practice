c6b2481908d32c5fb0dcc0df47b3295d98f20c16
Nicholas Flinn <huckflinn@users.noreply.github.com>
2025-05-12 08:20:33 -0400

    Begin Exercise 4

--- Exercises/Exercise-4/main.py
+++ Exercises/Exercise-4/main.py
@@ -1,9 +1,20 @@
-import boto3
+import json
+import csv
+from pathlib import Path
+
+import flatten_json
 
 
 def main():
-    # your code here
-    pass
+    p = Path(".")
+
+    json_files = list(p.glob("**/*.json"))
+
+    for json_file in json_files:
+        with open(json_file, encoding = "utf-8") as f:
+            json_tmp = json.load(f)
+
+        print(f"{json_file.name}: {json_tmp}")
 
 
 if __name__ == "__main__":
