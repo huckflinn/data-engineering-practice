8ce76ea6926f149036c9314fdb05d73cc570ddd3
Nicholas Flinn <huckflinn@users.noreply.github.com>
2025-05-12 21:34:13 -0400

    Add flatten_json and csv functionality

--- Exercises/Exercise-4/main.py
+++ Exercises/Exercise-4/main.py
@@ -2,19 +2,37 @@ import json
 import csv
 from pathlib import Path
 
-import flatten_json
+from flatten_json import flatten
 
 
 def main():
     p = Path(".")
 
+    csv_dir = Path("./csv_data")
+
+    if not csv_dir.exists():
+        csv_dir.mkdir()
+
     json_files = list(p.glob("**/*.json"))
 
     for json_file in json_files:
-        with open(json_file, encoding = "utf-8") as f:
-            json_tmp = json.load(f)
+        with open(json_file, encoding = "utf-8") as f_json:
+            json_tmp = json.load(f_json)
 
-        print(f"{json_file.name}: {json_tmp}")
+            flat_json = flatten(json_tmp)
+
+            csv_name = json_file.name.split(".json")[0]
+
+        with open(f"{csv_dir}/{csv_name}.csv", 'w', encoding = "utf-8") as f_csv:
+            writer = csv.DictWriter(f_csv, fieldnames = flat_json.keys())
+
+            writer.writeheader()
+            
+            row = {}
+            for k, v in flat_json.items():
+                row[k] = v
+
+            writer.writerow(row)
 
 
 if __name__ == "__main__":
